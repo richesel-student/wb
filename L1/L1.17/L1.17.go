979647454ee054069d3a198d41f8afe5a7f1cc78
richesel-student <rinatdevlieamov@gmail.com>
2025-12-04 20:45:22 +0300

    L1.17

--- L1/L1.17/L1.17.go
+++ L1/L1.17/L1.17.go
@@ -1,20 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func binarySearch(target int, arrInt []int) bool {
+func binarySearch(target int, arr []int) int {
 
-	last := len(arrInt) - 1
+	last := len(arr) - 1
 	first := 0
 
 	for first <= last {
+
 		middele := (last + first) / 2
 
-		if arrInt[middele] == target {
-			fmt.Println(arrInt[middele])
-			return true
+		if arr[middele] == target {
+			return middele
 		} else {
-			if arrInt[middele] < target {
+			if arr[middele] < target {
 				first = middele + 1
 			} else {
 				last = middele - 1
@@ -22,24 +24,20 @@ func binarySearch(target int, arrInt []int) bool {
 		}
 
 	}
-	return false
+	return -1
 
 }
 
 func main() {
-	// left:=[]int{}
+
 	var arrInt []int
-	// arrNum:=[]int{}
 
 	for i := 25; i < 100; i++ {
 		if i%2 == 0 {
 			arrInt = append(arrInt, i)
 		}
 	}
-
-	// fmt.Println(arrNum)
-
-	target := 26
+	target := 28
 
 	search := binarySearch(target, arrInt)
 	fmt.Print(search)
