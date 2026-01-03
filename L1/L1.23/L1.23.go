ea64f7db7e5477a6b26c76cf9e6e624f306beecb
richesel-student <rinatdevlieamov@gmail.com>
2026-01-03 22:05:00 +0300

    L1.23

--- L1/L1.23/L1.23.go
+++ L1/L1.23/L1.23.go
@@ -1,54 +1,31 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"math/big"
 )
 
-type Value struct {
-	A *big.Int
-	B *big.Int
-}
+func DeleteIndex(arr []int, index int) ([]int, error) {
 
-func (v *Value) Add() *big.Int {
-
-	return new(big.Int).Add(v.A, v.B)
-}
-
-func (v *Value) Sub() *big.Int {
-
-	return new(big.Int).Sub(v.A, v.B)
-}
-
-func (v *Value) Mul() *big.Int {
-
-	return new(big.Int).Mul(v.A, v.B)
-
-}
-
-func (v *Value) Div() (*big.Int, error) {
-
-	if len(v.B.Bits()) == 0 {
-		return nil, errors.New("Деление на ноль")
-
+	if index < 0 || index >= len(arr) {
+		return arr, fmt.Errorf("Нет элемента в списке")
 	}
-
-	return new(big.Int).Div(v.A, v.B), nil
-
+	copy(arr[index:], arr[index+1:])
+	arr[len(arr)-1] = 0
+	arr = arr[:len(arr)-1]
+	return arr, nil
 }
 
 func main() {
-	numbers := Value{A: big.NewInt(1 << 27), B: big.NewInt(1 << 27)}
-	fmt.Printf("\nСумма двух чисел:%v", numbers.Add())
-	fmt.Printf("\nРазность двух чисел:%v", numbers.Sub())
-	fmt.Printf("\nУмножение двух чисел:%v", numbers.Mul())
-
-	res, err := numbers.Div()
+	arrInt := make([]int, 0, 99)
+	for i := 1; i < 100; i++ {
+		arrInt = append(arrInt, i)
+	}
+	del, err := DeleteIndex(arrInt, 1)
 	if err != nil {
-		fmt.Print("\nОшибка:", err)
-	} else {
-		fmt.Println("\nДеление двух чисел:", res)
+		fmt.Print("Ошибка:", err)
 
 	}
+	fmt.Print(del)
+
 }
+
